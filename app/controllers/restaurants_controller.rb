947e1c6dd958febbcfa148998af1053d76201fd3
brechen <zappeln@gmail.com>
2016-01-31 21:44:10 -0600

    successfully creates restaurant and populates db

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -15,4 +15,14 @@ class RestaurantsController < ApplicationController
   def new
     @restaurant = Restaurant.new
   end
+
+  def create
+    Restaurant.create(restaurant_params)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :score, :body, :latitude, :longitude)
+  end
 end
