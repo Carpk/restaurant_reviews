bc19d4d4ebd3256250d3c3dfb9757f6b02a78e54
brechen <zappeln@gmail.com>
2016-02-07 20:56:25 -0600

    routing for category creation

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,14 +10,20 @@ class CategoriesController < ApplicationController
   end
 
   def create
-    Category.create(set: params[:category])
+    Category.create(category_params)
+
+    redirect_to admin_path(1)
+  end
+
+  def destroy
+    Category.find(params[:id]).destroy
 
     redirect_to admin_path(1)
   end
 
   private
 
-  # def category_params
-  #   params.require(:category).permit(:set)
-  # end
+  def category_params
+    params.require(:category).permit(:set, :description)
+  end
 end
