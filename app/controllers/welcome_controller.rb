e8126e0b13b114286e6205e413d93fe67358d420
brechen <zappeln@gmail.com>
2016-02-08 16:17:20 -0600

    add features on landing page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,8 @@
 class WelcomeController < ApplicationController
 
   def index
+    @featured_list = Restaurant.last(4)
     @categories = Category.all
-    @latest = Review.last
+    @latest_reviews = Review.last(4)
   end
 end
