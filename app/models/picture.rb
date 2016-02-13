6507189d3efe145b79914c44df49fa227a2035e6
brechen <zappeln@gmail.com>
2016-02-13 15:48:16 -0600

    default picture rename for heroku push

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,6 +2,6 @@ class Picture < ActiveRecord::Base
   belongs_to :review
 
   def self.default_pic
-    "/assets/default.jpg"
+    "/assets/default-3d5292881338b54f6d6b71bab276827a566a8dbfe26c4a493841bedcc793a96f.jpg"
   end
 end
