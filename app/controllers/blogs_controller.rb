04304f0fda08b0cb7f2c5365ae3a48d65a6f5c69
brechen <zappeln@gmail.com>
2016-02-16 13:13:25 -0600

    able to create and delete blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,4 +7,23 @@ class BlogsController < ApplicationController
   def new
     @blog = Blog.new
   end
+
+  def create
+    Blog.create(blog_params)
+
+    redirect_to admin_path(1)
+  end
+
+  def destroy
+    Blog.find(params[:id]).destroy
+
+    redirect_to admin_path(1)
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :description)
+  end
 end
+
