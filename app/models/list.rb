42d0bc4a448b5023ebe8809a09d8e62a31ea4de0
brechen <zappeln@gmail.com>
2016-03-03 07:39:40 -0600

    List view on admin console will either list Restaurant or Review class

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,8 @@
 class List < ActiveRecord::Base
   has_and_belongs_to_many :reviews
   has_and_belongs_to_many :restaurants
+
+  def listed_items
+    self.reviews.empty? ? self.restaurants : self.reviews
+  end
 end
