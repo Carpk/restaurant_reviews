4fa18a469517a58253726375025262bc791c04d0
brechen <zappeln@gmail.com>
2016-05-19 21:14:25 -0500

    start structure for new landing page

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,8 +3,4 @@ class List < ActiveRecord::Base
   has_many :restaurant_listing
   has_many :reviews, :through => :review_listing
   has_many :restaurants, through: :restaurant_listing
-
- # def listed_items
-  #  self.reviews.empty? ? self.restaurants : self.reviews
-  #end
 end
